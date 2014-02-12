c24b9b901c13f117e0b4c68a2e65ca2a44b44636
bentin.shai@gmail.com <bentin.shai@gmail.com@f44238aa-67e6-c9cc-ab7f-83b2a39e7442>
2014-02-12 15:58:22 +0000

    latest version

--- trunk/Jaqu/src/com/centimia/orm/jaqu/constant/Constants.java
+++ trunk/Jaqu/src/com/centimia/orm/jaqu/constant/Constants.java
@@ -27,7 +27,7 @@ package com.centimia.orm.jaqu.constant;
  */
 public class Constants {
 
-	public static final String BUILD_ID = "2.0.0";
+	public static final String BUILD_ID = "3.0.8";
 	public static final int IO_BUFFER_SIZE = 4 * 1024;
 	public static final String UTF8 = "UTF8";
 
