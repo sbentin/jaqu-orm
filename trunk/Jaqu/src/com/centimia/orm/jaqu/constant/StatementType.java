1b37a56ed42a3f1acf14459aa166bf27a90781da
bentin.shai@gmail.com <bentin.shai@gmail.com@f44238aa-67e6-c9cc-ab7f-83b2a39e7442>
2014-02-12 15:58:40 +0000

    enum for pojo statement type selection

---
+++ trunk/Jaqu/src/com/centimia/orm/jaqu/constant/StatementType.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2010-2016 Centimia Ltd.
+ * All rights reserved.  Unpublished -- rights reserved
+ *
+ * Use of a copyright notice is precautionary only, and does
+ * not imply publication or disclosure.
+ *
+ * THIS SOFTWARE CONTAINS CONFIDENTIAL INFORMATION AND TRADE
+ * SECRETS OF CENTIMIA. USE, DISCLOSURE, OR
+ * REPRODUCTION IS PROHIBITED WITHOUT THE PRIOR EXPRESS
+ * WRITTEN PERMISSION OF CENTIMIA Ltd.
+ */
+
+/*
+ ISSUE			DATE			AUTHOR
+-------		   ------	       --------
+Created		   Feb 12, 2014			shai
+
+*/
+package com.centimia.orm.jaqu.constant;
+
+/**
+ * @author shai
+ *
+ */
+public enum StatementType {
+	INSERT,UPDATE,DELETE;
+}
