ff018df41120d8e0da219587745ee754c4222466
Shai Bentin <shai@centimia.com>
2015-06-13 09:51:09 +0300

    made this interface public so it can be actually used in StringFilter

--- Jaqu/src/com/centimia/orm/jaqu/ISelectTable.java
+++ Jaqu/src/com/centimia/orm/jaqu/ISelectTable.java
@@ -1,10 +1,27 @@
+/*
+ * Copyright (c) 2007-2010 Centimia Ltd.
+ * All rights reserved.  Unpublished -- rights reserved
+ *
+ * Use of a copyright notice is precautionary only, and does
+ * not imply publication or disclosure.
+ *  
+ * Multiple-Licensed under the H2 License,
+ * Version 1.0, and under the Eclipse Public License, Version 1.0
+ * (http://h2database.com/html/license.html).
+ * Initial Developer: H2 Group, Centimia Inc.
+ */
 package com.centimia.orm.jaqu;
 
 import java.util.Map;
 
-interface ISelectTable<T> {
+/**
+ * The current table in query
+ * @author shai
+ * @param <T>
+ */
+public interface ISelectTable<T> {
 
-	enum JOIN_TYPE {
+	public enum JOIN_TYPE {
 		OUTER_JOIN("OUTER JOIN"),
 		LEFT_OUTER_JOIN("LEFT OUTER JOIN"),
 		INNER_JOIN("INNER JOIN"), 
